b13de71432d17b2293d62004206608bf28073995
99002461 <roshan.kochgawe@ltts.com>
2020-11-12 10:46:21 +0530

    Added the Medical details w.r.t firebase database @99002457 do check the integration part

---
+++ Implemention/Drone-SoSapp-master/app/src/main/java/com/example/myapplication/Medical_Details.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Medical_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical__details);
+
+
+    }
+}
